fa85cccd41e69637b425d027cb32d9b3a2dfbe57
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-03-14 19:51:03 +0100

    3-1-g draw concentric circles

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,8 +2,8 @@
 import context from "./context.js";
 
 /**
- * 
- * @param {*} x1 x coordinate of starte line
+ * function to draw lines
+ * @param {*} x1 x coordinate of start line
  * @param {*} y1 y coordinate of start line
  * @param {*} x2 x coordinate of end line
  * @param {*} y2 y coordinate of end line
@@ -12,5 +12,17 @@ export function drawLine(x1, y1, x2, y2) {
     context.beginPath;
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
+    context.stroke();
+}
+
+/**
+ * function to stroke a circle
+ * @param {*} x x coordinate circle
+ * @param {*} y y coordinate circle
+ * @param {*} r radius circle
+ */
+export function strokeCircle(x, y, r) {
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2);
     context.stroke();
 }
