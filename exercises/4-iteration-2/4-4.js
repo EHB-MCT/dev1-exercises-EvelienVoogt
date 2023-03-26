d62d4ff9547d98451c63be3259fbe6b782065c7f
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-03-26 12:54:30 +0200

    4-4 Circles with random radius

--- exercises/4-iteration-2/4-4.js
+++ exercises/4-iteration-2/4-4.js
@@ -5,14 +5,15 @@ import * as Utils from "../../scripts/utils.js";
 drawCircles();
 
 function drawCircles() {
-    context.fillStyle = "lightblue";
-    context.strokeStyle = "lightblue";
+    context.fillStyle = Utils.hsla(195, 100, 50, 0.4);
+    context.strokeStyle = Utils.hsla(195, 100, 50, 0.0);
     let total = 10;
     for (let i= 0; i <= total; i++) {
         for (let j = 0; j <= total; j++) {
             let x = i * (innerWidth / 10);
             let y = j * (innerHeight / 10);
-            Utils.fillAndStrokeCircle(x, y, 35);
+            let radius = Math.floor(Math.random() * 60);
+            Utils.fillAndStrokeCircle(x, y, radius);
         }
     }
 }
