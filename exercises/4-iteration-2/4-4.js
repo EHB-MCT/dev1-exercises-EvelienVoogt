647d9a91b9ef3a11ec4dae72bc790ae5b84a2404
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-03-26 19:33:02 +0200

    4-4 circles, red or blue rows

--- exercises/4-iteration-2/4-4.js
+++ exercises/4-iteration-2/4-4.js
@@ -8,8 +8,8 @@ function drawCircles() {
     let total = 10;
     for (let i= 0; i <= total; i++) {
         for (let j = 0; j <= total; j++) {
-            context.fillStyle = Utils.hsla((i % 2) * 200, 100, 50, 0.4);
-            context.strokeStyle = Utils.hsla((i % 2) * 200, 100, 50, 0.0);
+            context.fillStyle = Utils.hsla((j % 2) * 200, 100, 50, 0.4);
+            context.strokeStyle = Utils.hsla((j % 2) * 200, 100, 50, 0.0);
             let x = i * (innerWidth / 10);
             let y = j * (innerHeight / 10);
             let radius = 35;
