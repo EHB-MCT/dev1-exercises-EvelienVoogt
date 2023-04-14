e34c0c95932f8af42bed0e020f8d11f7b6afbb9d
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-04-14 21:42:37 +0200

    5-2-Maze

---
+++ exercises/5-selection-input-events/5-2-maze.js
@@ -0,0 +1,26 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawMaze();
+
+function drawMaze() {
+    context.fillStyle = "darkgray";
+    context.lineCap = "square";
+    context.lineWidth = 5;
+    context.fillRect(0, 0, width, height);
+    let space = 10;
+    for (let i = 0; i < width / space; i++) {
+        for (let j = 0; j < height / space; j++) {
+            let direction = Utils.randomNumber(0, 1);
+            if (direction == 0) {
+                Utils.drawLine(i * space, j * space, (i + 1) * space, (j + 1) * space);
+            } else {
+                Utils.drawLine(i * space, (j + 1) * space, (i + 1) * space, j * space);
+            } 
+        }
+    }
+}
