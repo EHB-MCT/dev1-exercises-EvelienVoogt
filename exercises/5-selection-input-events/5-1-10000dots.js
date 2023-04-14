a60cd3b66a787f507d397bae64b4f8f1393e69d5
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-04-14 20:25:15 +0200

    5-1-blue and white dots

--- exercises/5-selection-input-events/5-1-10000dots.js
+++ exercises/5-selection-input-events/5-1-10000dots.js
@@ -17,10 +17,10 @@ function drawDots() {
     for (let i = 0; i < 10000; i++) {
         let x = Utils.randomNumber(0, width);
         let y = Utils.randomNumber(0, height);
-        if (x < width * 2 / 3 && y < height * 2 / 3) {
-            context.fillStyle = "green";
-        } else {
+        if (x > width / 4 && x < width * 3 / 4 && y > height / 4 && y < height * 3 / 4) {
             context.fillStyle = "white";
+        } else {
+            context.fillStyle = "blue";
         }
         Utils.fillCircle(x, y, 2);
     }
