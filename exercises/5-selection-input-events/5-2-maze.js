ba87aac3e90ce1bf075f9840894283e4a778e1cc
Evelien Voogt <evelien.voogt@student.ehb.be>
2023-04-14 21:58:09 +0200

    5-2-Maze with randomized colors

--- exercises/5-selection-input-events/5-2-maze.js
+++ exercises/5-selection-input-events/5-2-maze.js
@@ -8,7 +8,7 @@ let height = context.canvas.height;
 drawMaze();
 
 function drawMaze() {
-    context.fillStyle = "darkgray";
+    context.fillStyle = "black";
     context.lineCap = "square";
     context.lineWidth = 5;
     context.fillRect(0, 0, width, height);
@@ -16,6 +16,7 @@ function drawMaze() {
     for (let i = 0; i < width / space; i++) {
         for (let j = 0; j < height / space; j++) {
             let direction = Utils.randomNumber(0, 1);
+            context.strokeStyle = Utils.hsla(Utils.randomNumber(0, 360), 100, 50, 1.0);
             if (direction == 0) {
                 Utils.drawLine(i * space, j * space, (i + 1) * space, (j + 1) * space);
             } else {
